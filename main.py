6611e08db59812d00bd9deec35f61affae5e3729
garvingit <garvinmo@gmail.com>
2021-02-20 18:49:28 -0800

    copied basic code

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ Authors: Garvin Mo Zhen, Jason Leong, Brian Che
 Description: Discord Bot that Alerts Users Class Times
 """
 
-import discord 
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('ODEyODc1Mjk2MDg3NzM2MzIw.YDHHHQ.7gitUp6J6roujj0tyWaJ8fR5RFg')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
