e46b03b039fa0cd11b0f7b32c6f0b9ad49d71341
oJason725 <j_leong4@u.pacific.edu>
2021-02-20 19:27:55 -0800

    added message function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Description: Discord Bot that Alerts Users Class Times
 
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -18,5 +19,23 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
